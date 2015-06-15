dbe1fa2cd2b63de1f4583a71914f467beb3c6cb0
Lucas Quintana <lucassquintana@gmail.com>
2015-06-14 22:49:33 -0300

    LoginForm add validations

--- Login/LoginForm.cs
+++ Login/LoginForm.cs
@@ -18,43 +18,60 @@ namespace PagoElectronico.Login
 
         private void ingresar_Click(object sender, EventArgs e)
         {
-            String usernameText = username.Text;
-            String passwordText = password.Text;
+            string usernameText = username.Text;
+            string passwordText = password.Text;
 
             if ((usernameText == "") | (passwordText == ""))
             {
                 MessageBox.Show("Los campos Username y Password son obligatorios", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             } else {
                 //Get user
+                string consulta = "SELECT top 1 Usua_Codigo Usua_Password, Usua_Estado, Usua_Intentos FROM TIMEWARP.USUARIOS WHERE Usua_Username = " + usernameText;
+                Query qr = new Query(consulta);
+                DataTable table = (DataTable)qr.ObtenerDataTable();
+                int cant = table.Rows.Count;
                 //Validate if  User exist
-                if (true)
+                if (cant > 0)
                 {   
                     //Validate if enabled
-                    // ((Usua_Estado > 1) | (Usua_Intentos >= 3))
-                    if (true)
+                    // ((Usua_Estado < 1) | (Usua_Intentos >= 3))
+                    DataRow user = table.Rows[0];
+                    int estado = (int) user["Usua_Estado"];
+                    int intentos = (int) user["Usua_Intentos"];
+                    if ((estado < 1) | (intentos >= 0))
                     {
                         //Validate password
-                        if (true)
+                        string password_db = (string)user["Usua_Password"];
+                        if (password_db == passwordText)
                         {
-                            //Intento =0
+                            //Intento =0 UPDATE ???
                             //Validate cant de roles
+                            string codigo = (string)user["Usua_Codigo"];
+                            string consulta2 = "SELECT UxR_Rol, Rol_Descripcion FROM TIMEWARP.ROLES, TIMEWARP.ROL_USUARIOS WHERE UxR_Usuario = " + codigo + "UxR_Rol = Rol_Codigo";
+                            Query qr2 = new Query(consulta);
+                            DataTable tableRoles = (DataTable)qr2.ObtenerDataTable();
                             this.Visible = false;
-                            if (true) 
+                            if (table.Rows.Count > 1) 
                             {
                                 Home.MultiRolHome frm = new Home.MultiRolHome();
                                 frm.ShowDialog();
                             }
-                            //Rol unico Admin
-                            else if (false)
-                            {
-                                Home.AdminHome frm = new Home.AdminHome();
-                                frm.ShowDialog();
-                            }
-                            //Rol unico Cliente
                             else
                             {
-                                Home.ClientHome frm = new Home.ClientHome();
-                                frm.ShowDialog();
+                                DataRow rolRow = tableRoles.Rows[0];
+                                string rol = (string)rolRow["Rol_Descripcion"];
+                                //Rol unico Admin
+                                if (rol == "ADMINISTRADOR")
+                                {
+                                    Home.AdminHome frm = new Home.AdminHome();
+                                    frm.ShowDialog();
+                                }
+                                //Rol unico Cliente
+                                else
+                                {
+                                    Home.ClientHome frm = new Home.ClientHome();
+                                    frm.ShowDialog();
+                                }
                             }
                         }
                         else
